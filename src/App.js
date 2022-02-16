31c43286aea0ffc997d4256254394a346733b332
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-02-16 11:36:55 -0500

    Implements hooks to manage state. Spinner styles changed

--- src/App.js
+++ src/App.js
@@ -1,60 +1,57 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Scroll from "./components/scroll.component";
 import CardList from "./components/cardlist.component";
 import SearchBox from "./components/searchbox.component";
 import Spinner from "./components/spinner.component";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      searchField: "",
-    };
-  }
+function App() {
+  const [users, setUsers] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
-    fetch(
-      `https://randomuser.me/api/?results=20&nat=us,gb,fr,de&inc=name,email,picture`
-    )
-      .then((resp) => resp.json())
-      .then((userData) => {
-        return this.setState({
-          users: userData.results.map((user) => {
-            return {
-              firstName: user.name.first,
-              lastName: user.name.last,
-              email: user.email,
-              picture: user.picture.large,
-            };
-          }),
-        });
-      })
-      .catch((error) => console.log(error));
-  }
+  useEffect(() => {
+    getUserData();
+  }, []);
 
-  onSearchChange = (event) => {
-    return this.setState({ searchField: event.target.value });
+  const getUserData = async function () {
+    try {
+      const resp = await fetch(
+        `https://randomuser.me/api/?results=20&nat=us,gb,fr,de&inc=name,email,picture`
+      );
+      const userData = await resp.json();
+      return setUsers(
+        userData.results.map((user) => {
+          return {
+            firstName: user.name.first,
+            lastName: user.name.last,
+            email: user.email,
+            picture: user.picture.large,
+          };
+        })
+      );
+    } catch (error) {
+      return "Ooops";
+    }
   };
 
-  render() {
-    const { users, searchField } = this.state;
-    const filteredUsers = users.filter((user) => {
-      const fullName = `${user.firstName} ${user.lastName}`;
-      return fullName.toLowerCase().includes(searchField.toLowerCase());
-    });
-    return !users.length ? (
-      <Spinner />
-    ) : (
-      <div className="tc">
-        <h1>Friends Rolodex</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList users={filteredUsers} />
-        </Scroll>
-      </div>
-    );
-  }
+  const onSearchChange = (event) => {
+    return setSearchField(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) => {
+    const fullName = `${user.firstName} ${user.lastName}`;
+    return fullName.toLowerCase().includes(searchField.toLowerCase());
+  });
+  return !users.length ? (
+    <Spinner />
+  ) : (
+    <div className="tc">
+      <h1>Friends Rolodex</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList users={filteredUsers} />
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
